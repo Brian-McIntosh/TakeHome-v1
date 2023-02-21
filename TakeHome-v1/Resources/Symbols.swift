ad38b4a37af6d34a171d1998b8ba41ffe4be73ae
Brian McIntosh <brian@mixinteractive.com>
2023-02-21 18:38:41 -0500

    Added Symbols and Theme Enums
    Can't initialize an enum; gives us a type-safe namespace.

---
+++ TakeHome-v1/Resources/Symbols.swift
@@ -0,0 +1,15 @@
+//
+//  Symbols.swift
+//  TakeHome-v1
+//
+//  Created by Brian McIntosh on 2/21/23.
+//
+
+import SwiftUI
+// sf menu bar = 2.99
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
---
+++ TakeHome-v1/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  TakeHome-v1
+//
+//  Created by Brian McIntosh on 2/21/23.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let pill = Color("pill")
+    static let text = Color("text")
+}
