a82ff98c5af8e8180814e60c80a7d4da9bb7f391
Brian McIntosh <brian@mixinteractive.com>
2023-02-21 18:20:43 -0500

    Initial Commit

---
+++ TakeHome-v1/TakeHome_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHome_v1App.swift
+//  TakeHome-v1
+//
+//  Created by Brian McIntosh on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TakeHome_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
