64b5bbf2ccaf3f8c685beae246241796cb16edd2
Brian McIntosh <brian@mixinteractive.com>
2023-02-23 07:35:35 -0500

    Extracted reusable views like PillView

--- TakeHome-v1/Features/People/Views/PeopleView.swift
+++ TakeHome-v1/Features/People/Views/PeopleView.swift
@@ -15,47 +15,21 @@ struct PeopleView: View {
         
         NavigationView {
             ZStack {
-                Theme.background
-                    .ignoresSafeArea(edges: .top)
+                
+                background
+                
                 ScrollView {
                     
                     LazyVGrid(columns: columns,
                               spacing: 16) {
+                        
                         ForEach(0...5, id: \.self) { item in
-                            //Text("\(item): Hello, World!")
                             
-                            VStack(spacing: .zero) {
-                                Rectangle()
-                                    .fill(.blue)
-                                .frame(height: 130)
-                                
-                                VStack(alignment: .leading) {
-                                    Text("#\(item)")
-                                        .font(
-                                            .system(.caption, design: .rounded)
-                                            .bold()
-                                        )
-                                        .foregroundColor(.white)
-                                        .padding(.horizontal, 9)
-                                        .padding(.vertical, 4)
-                                        .background(Theme.pill, in: Capsule())
-                                    
-                                    Text("<First name> <Last name>")
-                                        .foregroundColor(Theme.text)
-                                        .font(
-                                            .system(.body, design: .rounded)
-                                        )
-                                }
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.horizontal, 8)
-                                .padding(.vertical, 5)
-                                .background(Theme.detailBackground)
-                            }
-                            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-                            .shadow(color: Theme.text.opacity(0.1),
-                            radius: 2,
-                                    x: 0,
-                                    y: 1)
+                            //Text("\(item): Hello, World!")
+                            //the point where we extracted our repeating VStack
+                            //replace w/ extracted component
+                            
+                            PersonItemView(user: item)
                         }
                     }
                     .padding()
@@ -78,30 +52,38 @@ struct PeopleView: View {
             .navigationTitle("People")
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {
-                    Button {
-                        //
-                    } label: {
-                        Symbols.plus
-                            .font(
-                                .system(.headline, design: .rounded)
-                                .bold()
-                            )
-                    }
-
+                    create
                 }
             }
-            
         }
-        
-        
-        
-        
-        
     }
 }
 
 struct PeopleView_Previews: PreviewProvider {
     static var previews: some View {
         PeopleView()
+    }
+}
+
+private extension PeopleView {
+    
+    //computed property
+    var background: some View {
+        Theme.background
+            .ignoresSafeArea(edges: .top)
+    }
+    
+    //computed property
+    var create: some View {
+        // move toolbar code here...
+        Button {
+            print("button extension")
+        } label: {
+            Symbols.plus
+                .font(
+                    .system(.headline, design: .rounded)
+                    .bold()
+                )
+        }
     }
 }
